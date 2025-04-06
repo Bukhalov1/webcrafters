93aa4ce835a9a17c0c17d818421049bceaf8cd80
bukhalov1 <buhalov.alexei@yandex.ru>
2025-04-06 21:03:18 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/static', express.static('static'));
+
+
+// Обработка формы
+app.post('/submit', (req, res) => {
+  const { name, email, phone, message } = req.body;
+
+  console.log('Получена заявка:');
+  console.log(`Имя: ${name}`);
+  console.log(`Email: ${email}`);
+  console.log(`Телефон: ${phone}`);
+  console.log(`Сообщение: ${message}`);
+
+  res.send('Заявка успешно отправлена!');
+});
+
+// Статическая папка (если HTML-файл хранится в public)
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
